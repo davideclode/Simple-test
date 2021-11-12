276a52f7551634cc19c2e80a4778fb9c823e3be3
Davide <50506878+davideclode@users.noreply.github.com>
2021-11-12 05:03:09 -0300

    chore: Final commit

---
+++ src/file.js
@@ -0,0 +1,9 @@
+function abrirMenu(){
+    document.getElementById('menu').style.width = '150px';
+    document.getElementById('conteudo').style.marginLeft = '150px'
+}
+
+function fecharMenu() {
+    document.getElementById('menu').style.width = '0px';
+    document.getElementById('conteudo').style.marginLeft = '0px'
+}
